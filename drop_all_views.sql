bc72e48d2715c6463bcfda267c5b355e8d464eab
CodyJD <49073904+CodyJD@users.noreply.github.com>
2019-04-01 11:51:51 -0700

    Add files via upload

---
+++ drop_all_views.sql
@@ -0,0 +1,15 @@
+--drop view vactivestudents;
+--drop view vallgrades;
+--drop view vclasstrack;
+--drop view  vgrades;
+--drop view vinactivestudents;
+--drop view  vmajors;
+--drop view  vstudents;
+--drop view  vyears ;
+
+drop table Resume;
+drop table Employee;
+drop table Store;
+drop table Product;
+drop table Supplier;
+drop table Member;
